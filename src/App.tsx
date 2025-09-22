56d332c02a229cd51f2c58a594796e89110e11fc
Nicolás Carrera <nicolascarrera42@gmail.com>
2025-09-21 21:17:48 -0500

    feat(app): add main app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
+import AppRoutes from "./routes"
+
 function App() {
 
-  return (
-    <>
-      <h1 className="font-bold leading-snug tracking-tight text-slate-800 mx-auto my-6 w-full text-2xl lg:max-w-3xl lg:text-5xl">
-        H1 Get ready to experience a new level of performance and functionality.
-      </h1>
-      <p className="font-normal leading-relaxed mx-auto text-slate-500 lg:text-lg text-base max-w-3xl">
-        The time is now for it to be okay to be great. For being a bright color.
-        For standing out. Get ready to experience a new level of performance and functionality.
-      </p>
-    </>
-  )
+  return <AppRoutes />
 }
 
 export default App
